9027958191a68e0cbf3444a289af8188da016532
rcmaddox <65855506+rcmaddox@users.noreply.github.com>
2022-10-20 22:10:49 -0500

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import Product from '../views/Product.vue'
+import Category from '../views/Category.vue'
+import Search from '../views/Search.vue'
+import SearchVue from '../views/Search.vue'
+
 const routes = [
   {
     path: '/',
@@ -14,6 +19,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+{
+  path: '/search',
+  name: 'Search',
+  component: Search,
+},
+
+  {
+    path: '/:category_slug/:product_slug',
+    name: 'Product',
+    component: Product
+  },
+  {
+    path: '/:category_slug/',
+    name: 'Category',
+    component: Category
   }
 ]
 
